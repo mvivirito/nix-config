ed7e2b21d7ba5ca532eea3c5a9620747e6b0782f
Michael Vivirito <mvivirito@gmail.com>
2024-05-17 18:06:35 -0700

    configuring hm

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -38,10 +38,9 @@
     };
   };
 
-  # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "michael";
+    homeDirectory = "/home/michael";
   };
 
   # Add stuff for your user as you see fit:
